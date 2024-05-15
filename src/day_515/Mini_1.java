26050e6705e832947b6baf70a8dba3998573c10e
Safiya2211 <90877845+Safiya2211@users.noreply.github.com>
2024-05-15 11:44:52 -0400

    5.3

--- src/day_515/Mini_1.java
+++ src/day_515/Mini_1.java
@@ -1,4 +1,19 @@
 package day_515;
 
 public class Mini_1 {
-}
+    public static void main(String[] args) {
+        System.out.println("Celsius         Fahrenheit");
+        double c = 0;
+        double f = 32;
+        for(int i = 0; i <= 50;i++){
+                System.out.println(" "+(int) c + "              " + f );
+
+            c+=2;
+            f= c*9/5+32;
+        }
+    }
+
+    }
+
+
+
